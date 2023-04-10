f1bb9fa24d3bf80dcb890c704599cf9fd1f8ed72
Zac <zac.princeton@gmail.com>
2023-04-10 10:22:28 -0500

    🔧 Fix analytics

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://0z.gg',
+    adapter: vercel({
+        analytics: true
+    })
+});
